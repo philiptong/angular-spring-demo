6baeab44a16c01d01016ced77664c4de1040b0e3
Philip Tong <philiptong@users.noreply.github.com>
2019-05-02 21:15:13 +0100

    Initial client side

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Spring Demo';
+  description = 'angular 7.2.0 + spring boot 2.1.4';
+  users = '';
+
+  constructor(private httpclient: HttpClient) {}
+
+  getUsers(): void {
+    this.httpclient.get<string>('/api/users').subscribe(
+      result => {
+        this.users = JSON.stringify(result);
+      },
+      error => {
+        alert('error: ' + error.message);
+      }
+    );
+  }
+}
